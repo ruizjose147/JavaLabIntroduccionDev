5838e43702c4198e450e35c5465f80b061f8baad
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-11 15:34:31 -0500

    solucion ejercicio 1 y 2

---
+++ src/co/com/dev_co/sofka/Ejercicio1.java
@@ -0,0 +1,25 @@
+package co.com.dev_co.sofka;
+
+public class Ejercicio1 {
+
+    int num1;
+    int num2;
+
+    public  void numeroMayor(int num1, int num2){
+
+        this.num1 = num1;
+        this.num2 = num2;
+
+        if(num1 > num2)
+            System.out.println(num1+ " es mayor que " + num2);
+        if(num1 < num2)
+            System.out.println(num2+ " es mayor que " + num1);
+        if(num1 == num2)
+            System.out.println(num1 + " Es igual a " + num2);
+    }
+
+    public static void main(String[] args) {
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        ejercicio1.numeroMayor(10, 100);
+    }
+}
